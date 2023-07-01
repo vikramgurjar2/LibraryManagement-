aa5cf0a226a4a44a3a56fbe61136fac6b76b2b0e
anurag <anuragraushan373@gmail.com>
2023-07-01 15:59:05 +0530

    url commited

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: "https://librarymanagementfrontend.onrender.com", // <-- location of the react app were connecting to
+        origin: "http://localhost:3000", // <-- location of the react app were connecting to
         credentials: true,
     })
 );
