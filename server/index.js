6e8e8a88094ad450d6b33e2c3994cfcf912c85ed
anurag <anuragraushan373@gmail.com>
2023-07-01 15:57:05 +0530

    url commited

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: "https://adorable-maamoul-4751c8.netlify.app/", // <-- location of the react app were connecting to
+        origin: "https://librarymanagementfrontend.onrender.com/", // <-- location of the react app were connecting to
         credentials: true,
     })
 );
